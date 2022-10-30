ed733e5eaf45da7f0320c4ea5fbe643d8f138c7d
LuisUcla <caluigi7@gmail.com>
2022-10-30 14:57:23 -0400

    funcion para salir de la app en el boton de atras del movil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { App } from '@capacitor/app';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private platform: Platform) {}
+
+  ngOnInit(): void {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(async () => {
+      setTimeout(() => {
+        //SplashScreen.hide();
+      }, 3000);
+
+      this.platform.backButton.subscribeWithPriority(-1, () => { // prioridad para salir de la app en el boton back button hardware
+        App.exitApp();
+      });
+    })
+  }
+
 }
