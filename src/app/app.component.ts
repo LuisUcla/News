5e6c007a55b6680e980f97b2bfc7b0cf506df7ab
LuisUcla <caluigi7@gmail.com>
2022-10-30 15:25:30 -0400

    alerta previa antes de salir de la app pulsando el boton de atras del movil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { App } from '@capacitor/app';
-import { Platform } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,7 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private platform: Platform) {}
+  constructor(private platform: Platform, private alertCtrl: AlertController) {}
 
   ngOnInit(): void {
     this.initializeApp();
@@ -21,9 +21,34 @@ export class AppComponent implements OnInit {
       }, 3000);
 
       this.platform.backButton.subscribeWithPriority(-1, () => { // prioridad para salir de la app en el boton back button hardware
-        App.exitApp();
+        this.presentAlert();
       });
     })
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: '¡Salir!',
+      message: 'Desea salir de la app?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            // pass
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            App.exitApp();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
 }
